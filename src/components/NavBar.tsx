6d0f748c62ead84fe5a1d7e254545adcbd14f2a7
Siyana Pavlova <siyana.pavlova@gmail.com>
2026-03-07 00:23:44 +0100

    Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,18 @@
-import { Center, Flex, Heading } from "@chakra-ui/react";
+import { Center, Flex, Heading, HStack, Image } from "@chakra-ui/react";
 import "./navbar.css";
 import About from "./About";
+import logo from "../assets/logo.png";
 
 const NavBar = () => {
   return (
     <Flex justify="space-between">
+      <HStack padding={2}>
+        <Image src={logo} alt="Twinning BG Logo" height="25px" marginLeft={2} />
+        <Heading>Twinning BG</Heading>
+      </HStack>
       <Center>
-        <Heading marginX={5}>Twinning BG</Heading>
+        <About />
       </Center>
-      <About />
     </Flex>
   );
 };
