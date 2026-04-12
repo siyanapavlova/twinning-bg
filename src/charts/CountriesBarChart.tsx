cc2a277a1de0ff3a291392dfbc342d2abeab9d67
Siyana Pavlova <siyana.pavlova@gmail.com>
2026-04-12 19:46:49 +0200

    Bar chart of towns and relations (together) per country done

--- src/charts/CountriesBarChart.tsx
+++ src/charts/CountriesBarChart.tsx
@@ -7,25 +7,25 @@ import {
   Tooltip,
   Legend,
 } from "recharts";
-// import { RechartsDevtools } from '@recharts/devtools';
 import {
   townNumberPerCountry,
   relationNumberPerCountry,
 } from "@/components/TwinTownsMap";
 
-const CountriesBarChart = () => {
-  const data = Object.entries(townNumberPerCountry)
-    .sort((a, b) => b[1] - a[1])
-    .map(([country, count]) => ({
-      country: country,
-      towns: count,
-    }));
+const data = Object.entries(relationNumberPerCountry)
+  .sort((a, b) => b[1] - a[1])
+  .map(([country, relationCount]) => ({
+    country,
+    relations: relationCount,
+    towns: townNumberPerCountry[country] || 0,
+  }));
 
+const CountriesBarChart = () => {
   return (
     <BarChart
       style={{
         width: "100%",
-        maxWidth: "1000px",
+        maxWidth: "2000px",
         maxHeight: "70vh",
         aspectRatio: 1.618,
       }}
@@ -33,30 +33,45 @@ const CountriesBarChart = () => {
       data={data}
       margin={{
         top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
+        right: 20,
+        left: 10,
+        bottom: 100,
       }}
+      barGap={200}
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis
         dataKey="country"
         angle={-90}
         textAnchor="end"
+        interval={0}
+        style={{
+          fontSize: "0.8rem",
+        }}
+        tick={{ dx: -6, dy: 5 }}
+      />
+      <YAxis
+        width="auto"
         style={{
           fontSize: "0.8rem",
         }}
       />
-      <YAxis width="auto" />
-      <Tooltip />
-      <Legend />
+      <Tooltip cursor={false} />
+      <Legend verticalAlign="top" height={36} />
       <Bar
-        dataKey="towns"
+        dataKey="relations"
+        name="Relations"
         fill="#007878"
-        activeBar={{ fill: "gold", stroke: "purple" }}
+        activeBar={{ fill: "#F7C860" }}
         radius={[10, 10, 0, 0]}
       />
-      {/* <RechartsDevtools /> */}
+      <Bar
+        dataKey="towns"
+        name="Towns"
+        fill="#00acac"
+        activeBar={{ fill: "#F0F0C8" }}
+        radius={[10, 10, 0, 0]}
+      />
     </BarChart>
   );
 };
