f11910d88985851758530023f47289fc97741ea9
Siyana Pavlova <siyana.pavlova@gmail.com>
2026-04-12 22:58:26 +0200

    Added titles to charts

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,5 +1,5 @@
 import CountriesBarChart from "@/charts/CountriesBarChart";
-import { Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading } from "@chakra-ui/react";
 import {
   relationNumberPerCountry,
   townNumberPerCountry,
@@ -22,22 +22,32 @@ const towns = Object.entries(townNumberPerCountry)
 
 const Stats = () => {
   return (
-    <>
-      <Heading>Stats</Heading>
+    <Flex direction="column" alignItems="center" padding={4} gap={8}>
+      <Heading color="gray.800" marginBottom={2}>
+        Number of twin relations and towns per country
+      </Heading>
       <CountriesBarChart />
+
+      <Heading color="gray.800" marginBottom={2}>
+        Number of twin relations per country
+      </Heading>
       <BarChartSingleProperty
         data={relations}
         propertyName="Relations"
         fillColor="#007878"
         activeBarColor="#F7C860"
       />
+
+      <Heading color="gray.800" marginBottom={2}>
+        Number of twinned towns per country
+      </Heading>
       <BarChartSingleProperty
         data={towns}
         propertyName="Towns"
         fillColor="#00acac"
         activeBarColor="#F0F0C8"
       />
-    </>
+    </Flex>
   );
 };
 
